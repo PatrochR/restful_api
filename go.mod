92690a6c4b236497f23accc12207ad077d5b3328
reza charsetad <rezacharsetad@outlook.com>
2025-04-12 10:42:16 +0330

    add auth

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require github.com/gin-gonic/gin v1.10.0
 
 require (
+	github.com/absagar/go-bcrypt v0.0.0-20171215093918-4b100ddf46d7
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -15,6 +16,7 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -34,6 +36,6 @@ require (
 	golang.org/x/text v0.24.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/sqlite v1.5.7 // indirect
-	gorm.io/gorm v1.25.12 // indirect
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.25.12
 )
--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
-type user struct {
+type User struct {
+	gorm.Model
 	Email    string `gorm:"unique"`
 	Password string
 }
@@ -19,21 +24,96 @@ func main() {
 	if err != nil {
 		panic("failed to connect database")
 	}
-	db.AutoMigrate(&user{})
+	db.AutoMigrate(&User{})
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
+	// register
+	router.POST("/auth/register", func(c *gin.Context) {
 		var body struct {
 			Email    string
 			Password string
 		}
 		if c.Bind(&body) != nil {
-			log.Fatal("error : binding a body!")
+			log.Println("error :register binding a body")
 			c.JSON(http.StatusBadRequest, gin.H{
 				"error": "something wrong!",
 			})
 			return
 		}
+
+		passwordHash, err := bcrypt.GenerateFromPassword([]byte(body.Password), 10)
+		if err != nil {
+			log.Println("error :user hash password")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "something wrong!",
+			})
+			return
+		}
+
+		user := User{Email: body.Email, Password: string(passwordHash)}
+		result := db.Create(&user)
+		if result.Error != nil {
+			log.Println("error :user creat")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "something wrong!",
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{})
+	})
+
+	//login
+	router.POST("/auth/login", func(c *gin.Context) {
+		var body struct {
+			Email    string
+			Password string
+		}
+		if c.Bind(&body) != nil {
+			log.Println("error :login binding a body")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "something wrong!",
+			})
+			return
+		}
+		var user User
+		db.First(&user, "Email = ?", body.Email)
+
+		if user.ID == 0 {
+			log.Println("error :login email is not find")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid email or password!",
+			})
+			return
+		}
+
+		err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+		if err != nil {
+			log.Println("error :login password is wrong")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "invalid email or password!",
+			})
+			return
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"sub": user.ID,
+			"exp": time.Now().Add(time.Hour * 24 * 30).Unix(),
+		})
+
+		// Sign and get the complete encoded token as a string using the secret
+		tokenString, err := token.SignedString([]byte(os.Getenv("SECRET")))
+		if err != nil {
+			log.Println("massage : " + err.Error())
+			log.Println("error :login generate token")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": "something wrong!",
+			})
+			return
+		}
+		c.JSON(http.StatusOK, gin.H{
+			"token": tokenString,
+		})
 	})
 	router.Run()
 }
